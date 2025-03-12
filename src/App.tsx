6e73ed1124480e9dc601e48d1df34bee1e74a580
Sahil Chhatbar <sahilchhatbar7@gmail.com>
2025-03-12 17:48:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <><div className='bg-[#fafafa]'>
+    <Sidebar/>
+    </div>
+    </>
+  );
+}
+
+export default App;
